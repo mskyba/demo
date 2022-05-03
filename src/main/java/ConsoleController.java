5a45fd5dbe1cc9782bbc78362fbd69ef6b7cb5c2
mskyba <maryanakozak123>
2022-05-03 09:37:49 +0300

    Calculator

---
+++ src/main/java/ConsoleController.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ConsoleController {
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        static void out(String value) {
+            System.out.println(value);
+
+        }
+        static void out(int value) {
+            System.out.println(value);
+
+        }
+        int readInt(){
+            return scanner.nextInt();
+        }
+    }
+
