277dd7a11a4cc726771ca4489ab9b5b50100ab8b
Mykola <mbskyba@gmail.com>
2022-05-05 10:26:54 +0300

    calc_commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main (String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("sum " + calculator.sum(2.3,3, 3));
+        System.out.println("dep " + calculator.dep(4,2.3));
+        System.out.println("mult " + calculator.mult(2.2,3.1));
+        System.out.println("divide "+ calculator.divide(2.1,4.2));
+
+//        Cat cat = new Cat();
+//
+//        cat.name = "Barsik";
+//        cat.say(1);
+//        cat.say("za");
     }
 }
